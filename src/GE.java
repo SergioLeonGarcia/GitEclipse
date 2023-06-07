10d5ee744e3bb78d192985c9b489da139d710ac9
Sergio Leon <sergioleongarcia1@gmail.com>
2023-06-07 09:10:57 +0200

    Root Commit

---
+++ src/GE.java
@@ -0,0 +1,13 @@
+
+public class GE {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String nombre = "Sergio";
+		String apellido = "León";
+		
+		System.out.println("Soy " + nombre +" y apellido " + apellido);
+	
+	}
+
+}
